8015508a7544b08ae70ff9eae0dcf65afc014cd7
patacca <patacca@autistici.org>
2023-02-19 22:43:25 +0100

    Add config file for server and add WordleServer

---
+++ edu/riccardomori/wordle/WordleServer.java
@@ -0,0 +1,19 @@
+package edu.riccardomori.wordle;
+
+/**
+ * This is the main server class
+ */
+public class WordleServer {
+    private int port;
+
+    public WordleServer(int port) {
+        this.port = port;
+        
+        // Initialize the state
+    }
+
+    public void run() {
+        // Init socket and listen on port X
+        // Each connection is handled by a threadpool
+    }
+}
