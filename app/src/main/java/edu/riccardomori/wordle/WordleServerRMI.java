080314f751a59a87cc2c4472d5c34708c4db7e0a
patacca <patacca@autistici.org>
2023-02-24 13:03:24 +0100

    Move folder structure to support gradle

---
+++ app/src/main/java/edu/riccardomori/wordle/WordleServerRMI.java
@@ -0,0 +1,19 @@
+package edu.riccardomori.wordle;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+/**
+ * This is the interface that defines which RMI methods are available
+ */
+public interface WordleServerRMI extends Remote {
+    /**
+     * Register a new user with the pair (username, password).
+     * 
+     * @param username The username
+     * @param password The password
+     * @return True if the registration was successful, false otherwise 
+     * @throws RemoteException
+     */
+    public boolean register(String username, String password) throws RemoteException;
+}
