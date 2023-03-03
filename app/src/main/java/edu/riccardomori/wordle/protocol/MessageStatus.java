a05dc106d625f5897730d78e973706d8f8e392b6
patacca <patacca@autistici.org>
2023-03-04 00:05:25 +0100

    Implement login action and restructure the code
    Now there's a separate subpackage `protocol` for those classes that
    conains informations about the protocol. That information is shared
    between the client and the server so it's in a separate package

---
+++ app/src/main/java/edu/riccardomori/wordle/protocol/MessageStatus.java
@@ -0,0 +1,33 @@
+package edu.riccardomori.wordle.protocol;
+
+public enum MessageStatus {
+    // @formatter:off
+    SUCCESS((byte) 1),
+    INVALID_USER((byte) 2),
+    ACTION_UNAUTHORIZED((byte) 3),
+    GENERIC_ERROR((byte) 0xff);
+    // @formatter:on
+
+    private final byte value;
+
+    private MessageStatus(byte value) {
+        this.value = value;
+    }
+
+    public static MessageStatus fromByte(byte value) {
+        switch (value) {
+            case 1:
+                return SUCCESS;
+            case 2:
+                return INVALID_USER;
+            case 3:
+                return ACTION_UNAUTHORIZED;
+            default:
+                return GENERIC_ERROR;
+        }
+    }
+
+    public byte getValue() {
+        return this.value;
+    }
+}
--- app/src/main/java/edu/riccardomori/wordle/server/WordleServerCore.java
+++ app/src/main/java/edu/riccardomori/wordle/server/WordleServerCore.java
@@ -1,16 +1,22 @@
 package edu.riccardomori.wordle.server;
 
+import java.nio.BufferUnderflowException;
 import java.nio.ByteBuffer;
 import java.nio.channels.SelectionKey;
+import java.nio.charset.StandardCharsets;
 import java.util.logging.Logger;
-import edu.riccardomori.wordle.Action;
 import edu.riccardomori.wordle.ClientState;
+import edu.riccardomori.wordle.protocol.Action;
+import edu.riccardomori.wordle.protocol.MessageStatus;
 import edu.riccardomori.wordle.server.WordleServer;
 
 public class WordleServerCore {
     private int interestOps; // The interest set of operations as a bitmask
     private ClientState state = ClientState.ANONYMOUS;
     private Logger logger;
+
+    private String username; // The username of the client who is running this session
+    private ByteBuffer writeBuf; // The buffer holding the writable data
 
     public WordleServerCore() {
         this.interestOps = SelectionKey.OP_READ;
@@ -23,35 +29,86 @@ public class WordleServerCore {
     }
 
     /**
-     * Read the message provided in the buffer as data coming from the client. It then performs the
-     * actions required considering the current state of the server. Note that the message **must**
-     * always be complete. A partial message results in undefined behavior.
+     * Utility function to prepare a message to send to the client and set the interestOps to
+     * OP_WRITE
+     * 
+     * @param code The return code that is set in the message
+     */
+    private void sendMessage(MessageStatus code) {
+        this.writeBuf = ByteBuffer.allocate(1);
+        this.writeBuf.put(code.getValue());
+        this.interestOps = SelectionKey.OP_WRITE;
+    }
+
+    /**
+     * Handler for the action login.
+     * 
+     * @param msg The message that has been sent by the client
+     */
+    private void loginHandler(ByteBuffer msg) {
+        this.logger.info("Action Login");
+
+        // Parse message
+        int usernameSize = msg.get();
+        int passwordSize = msg.get();
+        byte[] usernameRaw = new byte[usernameSize];
+        byte[] passwordRaw = new byte[passwordSize];
+        try {
+            msg.get(usernameRaw, 0, usernameSize);
+            msg.get(passwordRaw, 0, passwordSize);
+        } catch (BufferUnderflowException e) {
+            this.logger.finer("Malformed login message");
+            return;
+        }
+        String username = new String(usernameRaw, StandardCharsets.US_ASCII);
+        String password = new String(passwordRaw, StandardCharsets.US_ASCII);
+
+        this.logger.finest(String.format("user %s pass %s", username, password));
+
+        // Authenticate
+        if (WordleServer.getInstance().checkLogin(username, password)) {
+            this.logger.finer(String.format("User `%s` logged in", username));
+            this.state = ClientState.LOGGED;
+            this.username = username;
+
+            // Prepare the success message
+            this.sendMessage(MessageStatus.SUCCESS);
+        } else {
+            this.logger.finer(String.format("Authentication of user `%s` rejected", username));
+            // Prepare the auth rejected message
+            this.sendMessage(MessageStatus.INVALID_USER);
+        }
+    }
+
+    /**
+     * Read the message provided in the buffer and perform the action requested considering the
+     * current state of the session. Note that the message **must** always be complete. A partial or
+     * malformed message results in it being rejected.
      * 
      * @param buffer The buffer containing the message coming from the client. It must be ready to
      *        be read
      * @return The interest set of operations
      */
-    public int readMessage(ByteBuffer buffer) {
+    public int handleMessage(ByteBuffer buffer) {
+        this.logger.finest(String.format("Received a message of size %d", buffer.limit()));
+
         if (this.state == ClientState.ANONYMOUS) {
-            Action action = Action.fromByte(buffer.get());
-            if (action == Action.UNKNOWN) {
-                this.logger.info("Action Unknown");
-            }
+            switch (Action.fromByte(buffer.get())) {
+                case LOGIN:
+                    this.loginHandler(buffer);
+                    break;
 
-            if (action == Action.LOGIN) {
-                this.logger.info("Action Login");
-                String username = "user";
-                String password = "pass";
-
-                if (WordleServer.getInstance().checkLogin(username, password)) {
-                    this.logger.finer(String.format("User `%s` logged in", username));
-                    this.state = ClientState.LOGGED;
-                } else {
-                    this.logger
-                            .finer(String.format("Authentication of user `%s` rejected", username));
-                }
+                default:
+                    this.logger.info("Not allowed to perform this action");
+                    this.sendMessage(MessageStatus.ACTION_UNAUTHORIZED);
+                    break;
             }
+        } else {
+            this.logger.info(
+                    String.format("User `%s` not allowed to perform this action", this.username));
+            this.sendMessage(MessageStatus.ACTION_UNAUTHORIZED);
         }
+
         return this.interestOps;
     }
 }
