6971722308491b358c698b894b1f951f76c3d914
patacca <patacca@autistici.org>
2023-03-06 13:00:27 +0100

    [Server] Add initial sketch impl of send-word

--- app/src/main/java/edu/riccardomori/wordle/server/WordleServerCore.java
+++ app/src/main/java/edu/riccardomori/wordle/server/WordleServerCore.java
@@ -4,6 +4,10 @@ import java.nio.BufferUnderflowException;
 import java.nio.ByteBuffer;
 import java.nio.channels.SelectionKey;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.logging.Logger;
 import edu.riccardomori.wordle.protocol.Action;
 import edu.riccardomori.wordle.protocol.ClientState;
@@ -18,6 +22,7 @@ public class WordleServerCore {
     private String username; // The username of the client who is running this session
     private ByteBuffer writeBuf; // The buffer holding the writable data
     private String lastPlayedSecretWord; // Last played secret word
+    private int triesLeft;
 
     public WordleServerCore() {
         this.interestOps = SelectionKey.OP_READ;
@@ -67,6 +72,34 @@ public class WordleServerCore {
      */
     private void sendMessage(MessageStatus code) {
         this.sendMessage(code, ByteBuffer.allocate(0));
+    }
+
+    /**
+     * Same as {@code sendMessage(MessageStatus, ByteBuffer)} but put only the byte {@code value} in
+     * the {@code ByteBuffer}
+     * 
+     * @param code The return code that is set in the message
+     */
+    private void sendMessage(MessageStatus code, byte value) {
+        this.writeBuf.clear();
+        this.writeBuf.put(code.getValue());
+        this.writeBuf.put(value);
+        this.writeBuf.flip();
+        this.interestOps = SelectionKey.OP_WRITE;
+    }
+
+    /**
+     * Same as {@code sendMessage(MessageStatus, ByteBuffer)} but put only the long {@code value} in
+     * the {@code ByteBuffer}
+     * 
+     * @param code The return code that is set in the message
+     */
+    private void sendMessage(MessageStatus code, long value) {
+        this.writeBuf.clear();
+        this.writeBuf.put(code.getValue());
+        this.writeBuf.putLong(value);
+        this.writeBuf.flip();
+        this.interestOps = SelectionKey.OP_WRITE;
     }
 
     /**
@@ -134,13 +167,80 @@ public class WordleServerCore {
         // Update the session state
         this.state.play();
         this.lastPlayedSecretWord = secretWord;
+        this.triesLeft = WordleServer.getInstance().getWordTries();
 
         // Prepare the success message
-        ByteBuffer msg = ByteBuffer.allocate(Integer.BYTES*2);
+        ByteBuffer msg = ByteBuffer.allocate(Integer.BYTES * 2);
         msg.putInt(secretWord.length());
-        msg.putInt(WordleServer.getInstance().getWordTries());
+        msg.putInt(this.triesLeft);
         msg.flip();
         this.sendMessage(MessageStatus.SUCCESS, msg);
+    }
+
+    private void guessWordHandler(ByteBuffer msg) {
+        // No leftover tries
+        if (this.triesLeft == 0) {
+            // Send the time to the next secret word
+            this.sendMessage(MessageStatus.NO_TRIES_LEFT,
+                    WordleServer.getInstance().getNextSWTime());
+            return;
+        }
+
+        // Read the guessed word
+        String guessWord = StandardCharsets.UTF_8.decode(msg).toString();
+
+        this.logger.info(String.format("User `%s` guessed word `%s` (secret `%s`)", this.username,
+                guessWord, this.lastPlayedSecretWord));
+
+        // Invalid word
+        if (!WordleServer.getInstance().isValidWord(guessWord)) {
+            this.sendMessage(MessageStatus.INVALID_WORD, (byte) this.triesLeft);
+            return;
+        }
+
+        // Spend a try
+        this.triesLeft--;
+
+        // Client won
+        if (this.lastPlayedSecretWord.equals(guessWord)) {
+            // congratz
+            return;
+        }
+
+        // Generate hints
+        List<Integer> correct = new ArrayList<>();
+        List<Integer> partial = new ArrayList<>();
+        Map<Character, Integer> map = new HashMap<>();
+        for (int k = 0; k < this.lastPlayedSecretWord.length(); ++k) {
+            if (this.lastPlayedSecretWord.charAt(k) == guessWord.charAt(k)) {
+                correct.add(k);
+            } else {
+                int v = map.computeIfAbsent(this.lastPlayedSecretWord.charAt(k), i -> 0);
+                map.put(this.lastPlayedSecretWord.charAt(k), v + 1);
+            }
+        }
+        for (int k = 0; k < this.lastPlayedSecretWord.length(); ++k) {
+            if (this.lastPlayedSecretWord.charAt(k) != guessWord.charAt(k)) {
+                int c = map.getOrDefault(guessWord.charAt(k), 0);
+                if (c > 0) {
+                    partial.add(k);
+                    map.put(guessWord.charAt(k), c - 1);
+                }
+            }
+        }
+
+        // Forge message
+        ByteBuffer sMsg = ByteBuffer.allocate(3 + correct.size() + partial.size());
+        sMsg.put((byte) this.triesLeft);
+        sMsg.put((byte) correct.size());
+        sMsg.put((byte) partial.size());
+        for (int p : correct)
+            sMsg.put((byte) p);
+        for (int p : partial)
+            sMsg.put((byte) p);
+        sMsg.flip();
+
+        this.sendMessage(MessageStatus.SUCCESS, sMsg);
     }
 
     /**
@@ -186,6 +286,10 @@ public class WordleServerCore {
 
         } else if (this.state.isLogged() && this.state.isPlaying()) { // Logged and playing
             switch (Action.fromByte(buffer.get())) {
+                case SEND_WORD:
+                    this.guessWordHandler(buffer);
+                    break;
+
                 case LOGOUT:
                     this.logoutHandler();
                     break;
