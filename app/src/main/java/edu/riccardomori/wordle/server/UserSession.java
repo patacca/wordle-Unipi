7f0e799ce39bd2168371ced39912996d685982f4
patacca <patacca@autistici.org>
2023-03-10 14:13:49 +0100

    [Server] Redesign the server configuration
    The server persistent state now contains more informations other than the users.
    Add a GameDescriptor class to represent the stripped state of a completed game,
    that means it holds all the data needed to share the result of the game.
    Move out of ClientSession anything related to the user session, that instead
    belongs to the class UserSession.

--- app/src/main/java/edu/riccardomori/wordle/server/UserSession.java
+++ app/src/main/java/edu/riccardomori/wordle/server/UserSession.java
@@ -1,8 +1,55 @@
 package edu.riccardomori.wordle.server;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class UserSession {
     public boolean isActive = true; // Tells if the session is active or if it has been closed
     public String secretWord; // Last played secret word
+    public long gameId;
+    public int triesLeft;
+    public List<List<Integer>> correctHints = new ArrayList<>();
+    public List<List<Integer>> partialHints = new ArrayList<>();
 
     public UserSession() {}
+
+    public void addHint(String word) {
+        List<Integer> correct = new ArrayList<>();
+        List<Integer> partial = new ArrayList<>();
+        Map<Character, Integer> map = new HashMap<>();
+
+        // Check the correct ones and store the remeaining chars in map
+        for (int k = 0; k < this.secretWord.length(); ++k) {
+            if (this.secretWord.charAt(k) == word.charAt(k)) {
+                correct.add(k);
+            } else {
+                int v = map.computeIfAbsent(this.secretWord.charAt(k), i -> 0);
+                map.put(this.secretWord.charAt(k), v + 1);
+            }
+        }
+        // Compute the partials
+        for (int k = 0; k < this.secretWord.length(); ++k) {
+            if (this.secretWord.charAt(k) != word.charAt(k)) {
+                int c = map.getOrDefault(word.charAt(k), 0);
+                if (c > 0) {
+                    partial.add(k);
+                    map.put(word.charAt(k), c - 1);
+                }
+            }
+        }
+
+        this.correctHints.add(correct);
+        this.partialHints.add(partial);
+    }
+
+    public List<Integer> getLastCorrectHint() {
+        return Collections.unmodifiableList(this.correctHints.get(this.correctHints.size() - 1));
+    }
+    
+    public List<Integer> getLastPartialHint() {
+        return Collections.unmodifiableList(this.partialHints.get(this.partialHints.size() - 1));
+    }
 }
