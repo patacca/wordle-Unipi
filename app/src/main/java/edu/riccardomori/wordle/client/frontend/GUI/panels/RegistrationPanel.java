973e845a4c522faed65ad801277b26aa0ef52737
patacca <patacca@autistici.org>
2023-03-21 15:15:17 +0100

    [GUI] Clean code

--- app/src/main/java/edu/riccardomori/wordle/client/frontend/GUI/panels/RegistrationPanel.java
+++ app/src/main/java/edu/riccardomori/wordle/client/frontend/GUI/panels/RegistrationPanel.java
@@ -7,7 +7,6 @@ import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
-import java.awt.event.WindowEvent;
 import javax.swing.BorderFactory;
 import javax.swing.JPanel;
 import javax.swing.JPasswordField;
