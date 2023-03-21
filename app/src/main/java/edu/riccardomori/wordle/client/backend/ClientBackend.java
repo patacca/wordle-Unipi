b9383070a42909956722c58ba582b070cc9e75a3
patacca <patacca@autistici.org>
2023-03-21 20:13:38 +0100

    Small fix

--- app/src/main/java/edu/riccardomori/wordle/client/backend/ClientBackend.java
+++ app/src/main/java/edu/riccardomori/wordle/client/backend/ClientBackend.java
@@ -12,7 +12,6 @@ import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
-import java.rmi.server.ExportException;
 import java.rmi.server.UnicastRemoteObject;
 import java.util.ArrayList;
 import java.util.List;
